6444407d8b801ea4aac5ef6da94587bd15a1fcd4
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2024-11-22 18:41:27 +0900

    Add files via upload

---
+++ something.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+
+st.subheader("How has swearing in music changed over the past four decades?")
+st.write("Andrew Kornder and Goutham Yegappan :)")
