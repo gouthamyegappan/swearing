bb0e6f1a95abe33d721e1a59e8df3f00443378ec
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2024-11-22 19:23:26 +0900

    Update something.py

--- something.py
+++ something.py
@@ -2,6 +2,31 @@ import streamlit as st
 
 st.set_page_config(layout='wide')
 
+import pandas as pd
+from zipfile import ZipFile
+import pickle
+
+# Open the .zip file
+with ZipFile('data1.zip', 'r') as f:
+    # Extract all contents in the current directory
+    f.extractall()
+
+    # Open the .pkl file inside the zip
+    with f.open('data1.pkl') as pkl_file:
+        # Load the pickle data
+        data = pickle.load(pkl_file)
+
+# Check if the data is already a DataFrame
+if isinstance(data, pd.DataFrame):
+    df = data
+else:
+    # If not a DataFrame, attempt to convert
+    df = pd.DataFrame(data)
+
+# Display the DataFrame
+
+
 
 st.subheader("How has swearing in music changed over the past four decades?")
 st.write("Andrew Kornder and Goutham Yegappan :)")
+st.write(len(df))
