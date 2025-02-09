a1bddd02c05153b7638d82cb21f923808e3a5007
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-02-09 14:09:49 +0530

    Update Ranks.py

--- pages/Ranks.py
+++ pages/Ranks.py
@@ -17,6 +17,20 @@ songs = pd.read_csv('songs_ranked.csv', index_col = 0)
 rank_dta = songs.groupby("rank")['total_swear'].mean()
 
 fig2 = px.line(x=rank_dta.index, y=rank_dta, title="Rank's Impact on Swearing")
+# Best Fit Line
+slope, intercept = np.polyfit(rank_dta.index, rank_dta.values, 1)
+# Calculate fitted values
+fit_line = slope * rank_dta.index + intercept
+
+# Add the best fit line as a new trace
+fig2.add_trace(go.Scatter(
+    x=rank_dta.index,
+    y=fit_line,
+    mode='lines',
+    name='Best Fit Line',
+    line=dict(color='red', dash='dash')  # Optional styling: red, dashed line
+))
+
 fig2.update_layout(
     title={
         'text': "Rank's Impact on Swearing<br><sup>There seems to be a relationship between rank and number of swears</sup>",
@@ -34,12 +48,10 @@ new = rank_dta[-10:].sum()/10
 percent_change = round((new-old)/old, 3) * 100
 
 with col3:
-	st.write('In the chart to the right, there is a ', percent_change, "% more total number of swears on average out of the bottom 10 songs vs the top 10")
+	st.write(f'In the chart to the right, there is {percent_change}% more swears on average in the songs that reach ranks 90-100 compared to the ones that make it to 1-10. When looking at the best fit line we can see that the slope is {slope}, meaning that for each rank we drop there is on average that many more swears. Our y-intercept is {intercept}, meaning that songs making it to the number one spot, still have this many swears on average.')
 
 with col4:
 	st.plotly_chart(fig2)
-
-
 
 st.subheader("What percent of songs at each rank have N number of swear words?")
 number = st.number_input(
