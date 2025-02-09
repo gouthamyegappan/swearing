eaf25b02732b1d9533b1565b850d038a46ca621c
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-02-09 15:26:30 +0530

    Create Which Swear.py

---
+++ pages/Which Swear.py
@@ -0,0 +1,39 @@
+if 'final' in st.session_state:
+	final = st.session_state['final']
+	st.subheader("Correlation Matrix of Swears")
+	
+	final['fuck_total'] = final['fuck'] + final['motherfuck']
+	corr = final.drop_duplicates(subset = ['title', 'artist'], keep = 'first')[['shit', 'bitch', 'damn', 'dick', 'fuck_total', 'ass', 'hell']]
+	corr.corr()
+	
+	dataplot = sb.heatmap(corr.corr(),vmin = -0.1, vmax = 0.6, annot=True)
+	st.pyplot(dataplot.get_figure())
+	
+	j = pd.read_json('traj.json')
+	
+	counts = [j.iloc[1][0], j.iloc[1][1]]
+	words = list(j.iloc[0].values)
+	
+	with st.container(border=True):
+	
+			relative = st.checkbox("Measure by percents instead of absolute counts of swear words")
+			on = []
+			with st.container():
+					for word in words:
+							on.append(st.checkbox(word, key=word))
+	
+			counts = counts[int(relative)]
+	
+			ppy = go.Figure()
+	
+			x = list(range(1980, 2024))
+			for word, b in zip(words, on):
+					if not b:
+							continue
+	
+					y = counts[word]
+					ppy.add_trace(go.Scatter(x=x, y=y, mode='lines', name=word))
+	
+			ppy.update_layout(xaxis=dict(range=[x[0], x[-1]]))
+			st.plotly_chart(ppy, use_container_width=True)
+	st.subheader("What Percent of the 7 Are Used?")
