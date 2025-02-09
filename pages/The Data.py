14c8598e10670282c0dea22b51c76c5b085088e0
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-02-09 15:22:47 +0530

    Update The Data.py

--- pages/The Data.py
+++ pages/The Data.py
@@ -1,12 +1,12 @@
 if 'final' in st.session_state:
 	final = st.session_state['final']
     
-    dates = list(final['date'].unique())
-    option = st.selectbox(
-        "What date would you like data for?",
-        tuple(dates),
-    )
-    
-    user_data = final[final['date'] == option] # Filtering the dataframe.
-    st.dataframe(user_data)
+	dates = list(final['date'].unique())
+	option = st.selectbox(
+	"What date would you like data for?",
+	tuple(dates),
+	)
+	
+	user_data = final[final['date'] == option] # Filtering the dataframe.
+	st.dataframe(user_data)
 
