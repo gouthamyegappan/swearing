1a7c57f24eefd0cb7dbf4874b861d264b3280f8c
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-04-11 16:53:25 -0700

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -11,54 +11,62 @@ import json
 import plotly.graph_objects as go
 
 # Open the .zip file
-with ZipFile('data1 (2).zip', 'r') as f:
-    # Extract all contents in the current directory
-    f.extractall()
 
-    # Open the .pkl file inside the zip
-    with f.open('data1.pkl') as pkl_file:
-        # Load the pickle data
-        data1 = pd.read_pickle(pkl_file)
+def create_data():
+	with ZipFile('data1 (2).zip', 'r') as f:
+	    # Extract all contents in the current directory
+	    f.extractall()
+	
+	    # Open the .pkl file inside the zip
+	    with f.open('data1.pkl') as pkl_file:
+	        # Load the pickle data
+	        data1 = pd.read_pickle(pkl_file)
+	
+	# Open the .zip file
+	with ZipFile('data2.zip', 'r') as f:
+	    # Extract all contents in the current directory
+	    f.extractall()
+	
+	    # Open the .pkl file inside the zip
+	    with f.open('data2.pkl') as pkl_file:
+	        # Load the pickle data
+	        data2 = pd.read_pickle(pkl_file)
+	
+	# Open the .zip file
+	with ZipFile('data3.zip', 'r') as f:
+	    # Extract all contents in the current directory
+	    f.extractall()
+	
+	    # Open the .pkl file inside the zip
+	    with f.open('data3.pkl') as pkl_file:
+	        # Load the pickle data
+	        data3 = pd.read_pickle(pkl_file)
+	
+	# Open the .zip file
+	with ZipFile('data4.zip', 'r') as f:
+	    # Extract all contents in the current directory
+	    f.extractall()
+	
+	    # Open the .pkl file inside the zip
+	    with f.open('data4.pkl') as pkl_file:
+	        # Load the pickle data
+	        data4 = pd.read_pickle(pkl_file)
 
-# Open the .zip file
-with ZipFile('data2.zip', 'r') as f:
-    # Extract all contents in the current directory
-    f.extractall()
 
-    # Open the .pkl file inside the zip
-    with f.open('data2.pkl') as pkl_file:
-        # Load the pickle data
-        data2 = pd.read_pickle(pkl_file)
+	final = pd.concat([data1, data2, data3, data4])
+	final = pd.concat([final.iloc[:,0:6], final.iloc[:,11:]], axis = 1)
+	final['fuck_total'] = final['fuck'] + final['motherfuck']
 
-# Open the .zip file
-with ZipFile('data3.zip', 'r') as f:
-    # Extract all contents in the current directory
-    f.extractall()
-
-    # Open the .pkl file inside the zip
-    with f.open('data3.pkl') as pkl_file:
-        # Load the pickle data
-        data3 = pd.read_pickle(pkl_file)
-
-# Open the .zip file
-with ZipFile('data4.zip', 'r') as f:
-    # Extract all contents in the current directory
-    f.extractall()
-
-    # Open the .pkl file inside the zip
-    with f.open('data4.pkl') as pkl_file:
-        # Load the pickle data
-        data4 = pd.read_pickle(pkl_file)
-
-
-final = pd.concat([data1, data2, data3, data4])
-final = pd.concat([final.iloc[:,0:6], final.iloc[:,11:]], axis = 1)
-final['fuck_total'] = final['fuck'] + final['motherfuck']
+	return final
 
 
 # Store values in session state
 if 'final' not in st.session_state:
+	final = create_data()
 	st.session_state['final'] = final
+
+else:
+	final = st.session_state['final']
 
 st.set_page_config(layout='wide')
 
