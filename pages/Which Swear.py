49a88c245b7d989cba8e5c473d82e90191f3d804
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-09-02 22:01:59 -0400

    Update Which Swear.py

--- pages/Which Swear.py
+++ pages/Which Swear.py
@@ -1,56 +1,63 @@
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
-import plotly.express as px
-from zipfile import ZipFile
-import pickle
-import numpy as np
-import seaborn as sb
-import json
 import plotly.graph_objects as go
+import seaborn as sb
+import matplotlib.pyplot as plt  # needed to close figures
+import json
 
 st.subheader("Most Sweary Song Every Year")
-data = pd.read_csv('most_byyear.csv', index_col = 0)
-st.dataframe(data)
 
-if 'final' in st.session_state:
-	final = st.session_state['final']
-	st.subheader("Correlation Matrix of Swears")
-	
-	corr = final.drop_duplicates(subset = ['title', 'artist'], keep = 'first')[['shit', 'bitch', 'damn', 'dick', 'fuck_total', 'ass', 'hell']]
-	corr.corr()
-	
-	dataplot = sb.heatmap(corr.corr(),vmin = -0.1, vmax = 0.6, annot=True)
-	st.pyplot(dataplot.get_figure())
-	
-	j = pd.read_json('traj.json')
-	
-	counts = [j.iloc[1][0], j.iloc[1][1]]
-	words = list(j.iloc[0].values)
-	
-	with st.container(border=True):
-	
-			relative = st.checkbox("Measure by percents instead of absolute counts of swear words")
-			on = []
-			with st.container():
-					for word in words:
-							on.append(st.checkbox(word, key=word))
-	
-			counts = counts[int(relative)]
-	
-			ppy = go.Figure()
-	
-			x = list(range(1980, 2024))
-			for word, b in zip(words, on):
-					if not b:
-							continue
-	
-					y = counts[word]
-					ppy.add_trace(go.Scatter(x=x, y=y, mode='lines', name=word))
-	
-			ppy.update_layout(xaxis=dict(range=[x[0], x[-1]]))
-			st.plotly_chart(ppy, use_container_width=True)
-	st.subheader("What Percent of the 7 Are Used?")
+@st.cache_data(ttl=None, max_entries=4, show_spinner=False)
+def load_most_by_year():
+    return pd.read_csv("most_byyear.csv", index_col=0)
+
+@st.cache_data(ttl=None, max_entries=2, show_spinner=False)
+def load_traj():
+    # traj.json looks like a 2-row structure you index with iloc
+    return pd.read_json("traj.json")
+
+data = load_most_by_year()
+st.dataframe(data, use_container_width=True)
+
+if "final" in st.session_state:
+    final = st.session_state["final"]
+    st.subheader("Correlation Matrix of Swears")
+
+    corr_cols = ["shit", "bitch", "damn", "dick", "fuck_total", "ass", "hell"]
+    corr_df = (
+        final.drop_duplicates(subset=["title", "artist"], keep="first")[corr_cols]
+        .corr()
+    )
+
+    # Seaborn/Matplotlib → render and CLOSE the figure
+    fig, ax = plt.subplots(figsize=(6, 4))
+    sb.heatmap(corr_df, vmin=-0.1, vmax=0.6, annot=True, ax=ax)
+    st.pyplot(fig, use_container_width=True)
+    plt.close(fig)  # <-- IMPORTANT: prevents figure handle leaks
+
+    j = load_traj()
+    counts = [j.iloc[1][0], j.iloc[1][1]]
+    words = list(j.iloc[0].values)
+
+    with st.container(border=True):
+        relative = st.checkbox("Measure by percents instead of absolute counts of swear words")
+        on = []
+        with st.container():
+            for word in words:
+                on.append(st.checkbox(word, key=word))
+
+        counts = counts[int(relative)]
+
+        ppy = go.Figure()
+        x = list(range(1980, 2024))
+        for word, b in zip(words, on):
+            if not b:
+                continue
+            y = counts[word]
+            ppy.add_trace(go.Scatter(x=x, y=y, mode="lines", name=word))
+
+        ppy.update_layout(xaxis=dict(range=[x[0], x[-1]]))
+        st.plotly_chart(ppy, use_container_width=True)
+
+st.subheader("What Percent of the 7 Are Used?")
