52e1605cb1a2b3a59dad344f6eb6793bb7e9df82
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-09-08 16:28:50 -0400

    Update Ranks.py

--- pages/Ranks.py
+++ pages/Ranks.py
@@ -15,7 +15,6 @@ st.set_page_config(layout='wide')
 st.subheader("How does the rank impact the amount of swearing we find?")
 col3, col4 = st.columns(2)
 
-
 songs = pd.read_csv('rank_impact_on_swearing.csv', index_col = 0)
 def get_rank_graph(col, title, yaxis):
     rank_dta = songs.groupby("rank")[col].mean()
@@ -56,18 +55,19 @@ def get_rank_graph(col, title, yaxis):
     new = rank_dta[-10:].sum()/10
     percent_change = round((new-old)/old, 3) * 100
 
-    return fig2, old, new, percent_change
+    return fig2, old, new, percent_change, slope
 
-fig2, old2, new2, percent_change2 = get_rank_graph('total_swear', "Rank's Impact on Average Number of Swears in a Song", "Average Number of Swears")
-fig3, old3, new3, percent_change3 = get_rank_graph('percent_swear', "Rank's Impact on Percentage of Swears in a Song", "Percent of Swears")
+fig2, old2, new2, percent_change2, slope2 = get_rank_graph('total_swear', "Rank's Impact on Average Number of Swears in a Song", "Average Number of Swears")
+fig3, old3, new3, percent_change3, slope3 = get_rank_graph('percent_swear', "Rank's Impact on Percentage of Swears in a Song", "Percent of Swears")
+
+st.write(f'The two charts below illustrate the relationship between a song’s rank and the prevalence of swearing in its lyrics. The chart on the left depicts the average number of swears per song, while the chart on the right reports the average percentage of words classified as swears. Both figures indicate a positive association between rank and the likelihood of swearing. In the left chart, the slope is {round(slope2, 2)}, suggesting that for each unit decrease in rank, the expected number of swears increases by approximately {round(slope2, 2)}.'
+)
 
 with col3:
 	st.plotly_chart(fig2)
 
 with col4:
 	st.plotly_chart(fig3)
-
-###st.write(f'In the chart to the right, there is {percent_change}% more swears on average in the songs that reach ranks 90-100 compared to the ones that make it to 1-10. When looking at the best fit line we can see that the slope is {round(slope, 2)}, meaning that for each rank we drop there is on average that many more swears. Our y-intercept is {round(intercept, 2)}, meaning that songs making it to the number one spot, still have this many swears on average.')
 
 st.subheader("What percent of songs at each rank have N number of swear words?")
 number = st.selectbox(
@@ -79,6 +79,12 @@ data = pd.read_csv('swear_words_by_rank.csv', index_col = 0)
 data.columns = data.columns.astype(int)
 num_swears = data[int(number)]
 fig3 = px.line(x=num_swears.index, y=num_swears, title="Rank's Impact on Swearing")
+fig3.update_layout(
+	xaxis_title="Rank",
+	yaxis_title="Percent of Songs"
+)
+
+st.write(f'The chart below shows the percentage of songs at each rank that contain at least the number of swears specified above.')
 st.plotly_chart(fig3)
 
 	
