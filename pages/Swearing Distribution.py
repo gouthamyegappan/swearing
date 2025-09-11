358d037127ec50b36d70b059f7f7d8a32b288023
gouthamyegappan <81385739+gouthamyegappan@users.noreply.github.com>
2025-09-11 16:40:15 -0400

    Update Swearing Distribution.py

--- pages/Swearing Distribution.py
+++ pages/Swearing Distribution.py
@@ -4,11 +4,16 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
 
-st.subheader("Trajectory of Swearing in Music Over Time Raw Value")
 @st.cache_data(ttl=None, max_entries=3, show_spinner=False)
 def load_percent_dist():
     df = pd.read_csv("percent_dist.csv", index_col=0)
     return df
+
+@st.cache_data(ttl=None, max_entries=3, show_spinner=False)
+def load_dist_swears():
+    with open("dist_swears.json", "r") as f:
+        fig = pio.from_json(f.read())
+    return fig
 
 # 2) Percent threshold chart
 st.subheader("Trajectory of Swearing in Music Over Time")
@@ -21,5 +26,10 @@ num = st.selectbox(
 s = percent_df[num]
 fig_pct = px.line(x=s.index, y=s, title=f"Percent of Songs with {num} number of swears")
 st.plotly_chart(fig_pct, use_container_width=True)
+
+st.subheader("Distribution of Swears in Songs that Have Swears")
+fig = load_dist_swears()
+st.plotly_chart(fig_pct, use_container_width=True)
+
 
 
